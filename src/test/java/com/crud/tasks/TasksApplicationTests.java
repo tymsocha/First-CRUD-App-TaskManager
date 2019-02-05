0b17b9847d1bc645ea893bb26f560af3f0b4249d
tymsocha <tymoteusz.socha@outlook.com>
2019-02-05 21:29:55 +0100

    Module 18.3 - Lombok tests

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -1,5 +1,7 @@
 package com.crud.tasks;
 
+import com.crud.tasks.domain.TaskDto;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,20 @@ public class TasksApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testLombok() {
+		//Given
+		TaskDto taskDto = new TaskDto((long) 1, "Test Lombok", "I want to break free");
+		//When
+		long id = taskDto.getId();
+		String title = taskDto.getTitle();
+		String content = taskDto.getContent();
+		//Then
+		Assert.assertEquals(1, id);
+		Assert.assertEquals("Test Lombok", title);
+		Assert.assertEquals("I want to break free", content);
 	}
 
 }
