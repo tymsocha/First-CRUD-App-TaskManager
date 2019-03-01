c92bba7c777e0568465163b265abf5c64705f997
tymsocha <tymoteusz.socha@outlook.com>
2019-03-01 18:03:14 +0100

    Heroku and postman

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -7,15 +7,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
+public class TasksApplication /*extends SpringBootServletInitializer*/ {
 
 	public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
 	}
 
-	@Override
+	/*@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(TasksApplication.class);
-	}
+	}*/
 }
 
