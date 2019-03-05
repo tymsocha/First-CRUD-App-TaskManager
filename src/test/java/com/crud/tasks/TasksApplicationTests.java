0fa2c794164a2c614b6e814befa9ea3e584afcd7
tymsocha <tymoteusz.socha@outlook.com>
2019-03-05 20:35:35 +0100

    Module 22.1 started

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -7,13 +7,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class TasksApplicationTests {
 
-	/*@Test
+	@Test
 	public void contextLoads() {
 	}
 
